b06f7a7d64ebfca75ba02f60c79d19a212973d8f
Caio Damasceno Alves <caio.damasceno@aluno.ufop.edu.br>
2023-01-11 15:14:47 -0300

    Modelo inicial de Pessoa desenvolvido

---
+++ Atividade-Pratica_I/API/src/models/pessoa.model.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const pessoaSchema = new Schema({
+  nome: {
+    type: String,
+    required: true
+  },
+  rua: {
+    type: String,
+    required: true
+  },
+  numero: {
+    type: Number,
+    required: true
+  },
+  complemento: {
+    type: String
+  },
+  documento: {
+    type: String,
+    required: true
+  },
+  cidade: {
+    type: Schema.Types.ObjectId,
+    ref: 'Cidade'
+  },
+  tipo_sanguineo: {
+    type: Schema.Types.ObjectId,
+    ref: 'TipoSanguineo'
+  },
+  timestamp: true,
+  collerction: 'pessoas'
+});
+
+const Pessoa = mongoose.model('Pessoa', PessoaSchema);
+module.exports = Pessoa;
