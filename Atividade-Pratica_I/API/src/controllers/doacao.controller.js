038ddd98bd28a0c7031f555617aa7e68022e8db4
Caio Damasceno Alves <caio.damasceno@aluno.ufop.edu.br>
2023-01-11 20:29:57 -0300

    Dev: method and routes updateDoacaoById; Res: 200 | Postman

--- Atividade-Pratica_I/API/src/controllers/doacao.controller.js
+++ Atividade-Pratica_I/API/src/controllers/doacao.controller.js
@@ -48,3 +48,25 @@ exports.getDoacaoById = async (req, res) => {
         });
     }
 };
+
+exports.updateDoacaoById = async (req, res) => {
+    try {
+        const doacao = await Doacao.findByIdAndUpdate(req.params.id, req.body, { new: true });
+
+        if (!doacao) {
+            return res.status(404).send({
+                message: "Doação não encontrada com o id " + req.params.id
+            });
+        }
+        res.send(doacao);
+    } catch (err) {
+        if (err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Doação não encontrada com o id " + req.params.id
+            });
+        }
+        return res.status(500).send({
+            message: "Erro ao atualizar doação com o id " + req.params.id
+        });
+    }
+};
